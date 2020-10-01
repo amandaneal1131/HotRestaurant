f6f31bcdc0d25d622ee0c7d4560e13825e24ef30
Amanda Neal <68088863+amandaneal1131@users.noreply.github.com>
2020-09-30 19:56:35 -0500

    Add table data

---
+++ table-data.js
@@ -0,0 +1,8 @@
+var tableArray = [{
+    customerName: 'Amanda',
+    customerEmail: 'amandaneal1131@gmail.com',
+    customerID: 'The Boss',
+    phoneNumber: '555-5555'
+}];
+
+module.exports = tableArray;
